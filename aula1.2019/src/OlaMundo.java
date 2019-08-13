e2a1a369a936dfe086553714e102054ef2e7a493
Daniel5894 <31262371+Daniel5894@users.noreply.github.com>
2019-08-12 21:48:35 -0300

    Add files via upload

---
+++ aula1.2019/src/OlaMundo.java
@@ -0,0 +1,12 @@
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Olá Mundo!");
+		System.out.println();
+		System.out.println("Hello World!");
+		System.out.print("Aula 01");
+	}
+
+}
